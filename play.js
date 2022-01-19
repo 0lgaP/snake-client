86f5358f24fe220f58cef0c2ccbb3f763764ab92
0lgaP <perronolga@gmail.com>
2022-01-19 19:14:34 +0000

    added client.js and moved connect to client.js

--- play.js
+++ play.js
@@ -1,19 +1,9 @@
 const net = require('net');
+const {connect} = require('./client');
 
 //establish connection to game server
-const conn = net.createConnection({
-  host: 'localhost',
-  port: 50541
-});
+const conn = connect();
 
-const connect = function() {
-  conn.setEncoding('utf8');
-  return conn;
-};
 
 console.log("connection ...");
-connect();
 
-conn.on('data', (data) => {
-  console.log('Server says: ', data);
-});
